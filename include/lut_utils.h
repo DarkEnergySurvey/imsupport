8f7a5c3c6394497caab195cf35c9563ac4c7c1cb
rgruendl <rgruendl>
2013-05-24 19:12:01 +0000

    Cosmetic change because SVN glitched earlier.

--- include/lut_utils.h
+++ include/lut_utils.h
@@ -1,3 +1,3 @@
-/* lut_utils.c */
+/*  lut_utils.c  */
 void read_linearity_lut(char lutfile[], int ccdnum, float **lutx, double **luta, double **lutb);
 float lut_srch(const float value, const double lut[], const int flag_interp);
