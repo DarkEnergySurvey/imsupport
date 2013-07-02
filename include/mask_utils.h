14d5986df1a7ddd3b052b989a32413ffc8cb273e
daues <daues>
2013-07-02 18:52:15 +0000

    * Merging trunk changes to SV branch * Added mask_util subroutines and prototypes.  Updated make file so that imsupport library now contains these functions. (trunk r12412 rgruendl) * Updated Fixcol utility such that it properly resets mask bits when fixing a column or when detemining that fix is insignificant. (trunk  r12446 rgruendl) * version will be tagged 7.1.8 .

---
+++ include/mask_utils.h
@@ -0,0 +1,4 @@
+/*  mask_utils.c  */
+int colAve(int icol,short int bitson,desimage bpm,desimage output,double *pixavg,double *pixrms,double *pavgerr);
+int fixCol(desimage bpm,desimage output);
+void johnQsort(int gindex[],double value[], int last);
