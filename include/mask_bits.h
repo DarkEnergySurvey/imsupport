d3332033a90a0b1e434dad1fc6a32e03040cbb43
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-06-09 22:10:09 +0000

    Removing global variable bit definitions from the header (now just declarations)

--- include/mask_bits.h
+++ include/mask_bits.h
@@ -25,9 +25,17 @@ from despyfits import maskbits
 ...
 bit = maskbits.BADPIX_BPM
 
-The shared library access to this file is what necessitates the double
-definition of each bit.
+The shared library access to this file necessitates the external
+constant declaration of each bit. The bits are declared here but must
+be defined in:
+
+despyfits/src/libmaskbits.c
 */
+
+/***********************************************/
+/* NOTE: Bit definitions must be propagated to */
+/* despyfits/src/libmaskbits.c                 */
+/***********************************************/
 
 /*********************************************/
 /* Define BADPIX bit mappings (for MASK HDU) */
@@ -35,66 +43,64 @@ definition of each bit.
 
 // set in BPM (hot/dead pixel/column)
 #define BADPIX_BPM          1  
-const int badpix_bpm = BADPIX_BPM;
+extern const int badpix_bpm;
 // saturated pixel
 #define BADPIX_SATURATE     2  
-const int badpix_saturate = BADPIX_SATURATE;
+extern const int badpix_saturate;
 // interpolated pixel
 #define BADPIX_INTERP       4
-const int badpix_interp = BADPIX_INTERP;
+extern const int badpix_interp;
 // pixel on non-functional amplifier 
 #define BADPIX_BADAMP       8  
-const int badpix_badamp = BADPIX_BADAMP;
+extern const int badpix_badamp;
 // cosmic-ray pixel
 #define BADPIX_CRAY        16
-const int badpix_cray = BADPIX_CRAY;
+extern const int badpix_cray;
 // bright star pixel
 #define BADPIX_STAR        32  
-const int badpix_star = BADPIX_STAR;
+extern const int badpix_star;
 // bleed trail pixel
 #define BADPIX_TRAIL       64  
-const int badpix_trail = BADPIX_TRAIL;
+extern const int badpix_trail;
 // edge bleed pixel
 #define BADPIX_EDGEBLEED  128  
-const int badpix_edgebleed = BADPIX_EDGEBLEED;
+extern const int badpix_edgebleed;
 // pixel potentially affected by xtalk from a super-saturated source
 #define BADPIX_SSXTALK    256  
-const int badpix_ssxtalk = BADPIX_SSXTALK;
+extern const int badpix_ssxtalk;
 // pixel flag to exclude CCD glowing edges
 #define BADPIX_EDGE       512  
-const int badpix_edge = BADPIX_EDGE;
+extern const int badpix_edge;
 // pixel associated with streak from a satellite, meteor, ufo...
 #define BADPIX_STREAK    1024  
-const int badpix_streak = BADPIX_STREAK;
+extern const int badpix_streak;
 // nominally useful pixel but not perfect
 #define BADPIX_SUSPECT   2048 
-const int badpix_suspect = BADPIX_SUSPECT;
+extern const int badpix_suspect;
 // corrected by pixcorrect
 #define BADPIX_FIXED     4096  
-const int badpix_fixed = BADPIX_FIXED;
+extern const int badpix_fixed;
 // suspect due to edge proximity
 #define BADPIX_NEAREDGE  8192  
-const int badpix_nearedge = BADPIX_NEAREDGE;
+extern const int badpix_nearedge;
 // suspect due to known tape bump
 #define BADPIX_TAPEBUMP 16384  
-const int badpix_tapebump = BADPIX_TAPEBUMP;
+extern const int badpix_tapebump;
 
 /* ADW 2015-06-01: Can these be removed? */
 
 /* RAG thinks that this file definition is on the cusp of being deletable  */
 // pixels less than this fraction of sky are filtered.
 // helps remove failed reads
-#define BADPIX_THRESHOLD 0.10
-//const float badpix_threshold = BADPIX_THRESHOLD;
+//#define BADPIX_THRESHOLD 0.10
 
 // a bad pixel that was fixed - DEPRECATED
-// ADW: Still used in mask_utils.c
-#define BADPIX_FIX  (BADPIX_SUSPECT)
-//const int badpix_fix = BADPIX_FIX;
+//#define BADPIX_FIX  (BADPIX_SUSPECT)
+//extern const int badpix_fix;
 
 // too little signal - DEPRICATED
 //#define BADPIX_LOW (BADPIX_BADAMP) 
-//const int badpix_low = BADPIX_LOW;
+//extern const int badpix_low;
 
 
 /****************************************/
@@ -103,52 +109,52 @@ const int badpix_tapebump = BADPIX_TAPEBUMP;
 
 //Pixels that are dull in the flats.
 #define BPMDEF_FLAT_MIN    1  
-const int bpmdef_flat_min = BPMDEF_FLAT_MIN;
+extern const int bpmdef_flat_min;
 //Pixels that are hot in the flats.
 #define BPMDEF_FLAT_MAX    2  
-const int bpmdef_flat_max = BPMDEF_FLAT_MAX;
+extern const int bpmdef_flat_max;
 //Pixels that are in the BPM for the flats.
 #define BPMDEF_FLAT_MASK   4  
-const int bpmdef_flat_mask = BPMDEF_FLAT_MASK;
+extern const int bpmdef_flat_mask;
 // Pixels that are hot in the biases.
 #define BPMDEF_BIAS_HOT    8  
-const int bpmdef_bias_hot = BPMDEF_BIAS_HOT;
+extern const int bpmdef_bias_hot;
 // Pixels that are warm in the biases.
 #define BPMDEF_BIAS_WARM  16  
-const int bpmdef_bias_warm = BPMDEF_BIAS_WARM;
+extern const int bpmdef_bias_warm;
 // Pixels that are in the BPM for the biases
 #define BPMDEF_BIAS_MASK  32  
-const int bpmdef_bias_mask = BPMDEF_BIAS_MASK;
+extern const int bpmdef_bias_mask;
 // Pixels that are downstream of a hot pixel in the bias.
 #define BPMDEF_BIAS_COL   64  
-const int bpmdef_bias_col = BPMDEF_BIAS_COL;
+extern const int bpmdef_bias_col;
 // Pixels on the glowing edges of the CCD.
 #define BPMDEF_EDGE      128  
-const int bpmdef_edge = BPMDEF_EDGE;
+extern const int bpmdef_edge;
 // Correctable pixels (usually downstream of hot pixels).
 #define BPMDEF_CORR      256  
-const int bpmdef_corr = BPMDEF_CORR;
+extern const int bpmdef_corr;
 // Imperfect calibration or excess noise, such as tape bumps.  
 // Ignore for highest-precision work.
 #define BPMDEF_SUSPECT   512  
-const int bpmdef_suspect = BPMDEF_SUSPECT;
+extern const int bpmdef_suspect;
 // Columns with charge redistribution in sky exposures.
 #define BPMDEF_FUNKY_COL 1024 
-const int bpmdef_funky_col = BPMDEF_FUNKY_COL;
+extern const int bpmdef_funky_col;
 // Outliers in stacked sky exposures.
 #define BPMDEF_WACKY_PIX 2048
-const int bpmdef_wacky_pix = BPMDEF_WACKY_PIX;
+extern const int bpmdef_wacky_pix;
 // Pixel on non-functional amplifier
 #define BPMDEF_BADAMP    4096 
-const int bpmdef_badamp = BPMDEF_BADAMP;
+extern const int bpmdef_badamp;
 // suspect due to edge proximity
 #define BPMDEF_NEAREDGE  8192
-const int bpmdef_nearedge = BPMDEF_NEAREDGE;
+extern const int bpmdef_nearedge;
 // suspect due to known tape bump
 #define BPMDEF_TAPEBUMP 16384  
-const int bpmdef_tapebump = BPMDEF_TAPEBUMP;
+extern const int bpmdef_tapebump;
 
 /* ADW 2015-06-01: Can this be removed? */
 // Generic bad pixel flag - DEPRECATED
 //#define BPMDEF_GENERIC (BPMDEF_BADAMP);
-//const int bpmdef_generic = BPMDEF_GENERIC; 
+//extern const int bpmdef_generic = BPMDEF_GENERIC; 
